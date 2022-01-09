dd6a199cbf46e24cbef7cdd4185e480270edaf01
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-01-09 21:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import test_pair_to_number
 import color_pair_to_string
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+  test_number_to_pair.test_number_to_pair(4, 'White', 'Brown')
+  test_number_to_pair.test_number_to_pair(5, 'White', 'Slate')
+  test_number_to_pair.test_pair_to_number('Black', 'Orange', 12)
+  test_number_to_pair.test_pair_to_number('Violet', 'Slate', 25)
+  test_number_to_pair.test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
