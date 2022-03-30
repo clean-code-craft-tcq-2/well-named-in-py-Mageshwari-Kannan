a00c7091a51598b60d45a1f937eb8eb299fe203e
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-03-30 11:31:10 +0530

    Update reference_manual.py

--- reference_manual.py
+++ reference_manual.py
@@ -5,9 +5,28 @@ from test_pair_to_number import *
 from color_pair_to_string import *
 from color_pair_to_string import MAJOR_COLORS
 from color_pair_to_string import MINOR_COLORS
+import matplotlib.pyplot as plt
 
 def reference_manual():
-    
-    for i, major in enumerate(MAJOR_COLORS):
-        for j, minor in enumerate(MINOR_COLORS):
-            print(f'{i * 5 + j} | {major} | {minor}')
+    color_pair_rows = []
+    color_pair_text = []
+    color_pair_colors = []
+    color_pair_columns = ["ColorCode","MajorColor_Name","MajorColor","MinorColor_Name","MinorColor"]
+
+    for majorColor in MAJOR_COLORS:
+        for minorColor in MINOR_COLORS:
+            colorCode = get_pair_number_from_color(majorColor,minorColor)
+            print("ColorCode: ",colorCode,"- MajorColor: "+majorColor," ; MinorColor: "+minorColor)
+            color_pair_rows.append(colorCode)
+            color_pair_text.append([str(colorCode), majorColor,"",minorColor,""])
+            if(minorColor =="Slate"): minorColor = "slategrey"
+            color_pair_colors.append(['White',"White",majorColor,"White",minorColor])
+
+    colorPair, colorPair_Axes = plt.subplots(figsize = (6,8),)
+
+    colorPair_Axes.xaxis.set_visible(False) 
+    colorPair_Axes.yaxis.set_visible(False)
+
+    colorPair_Axes.table(cellText=color_pair_text, rowLabels=color_pair_rows, colLabels=color_pair_columns, cellColours=color_pair_colors, loc='top', bbox=[0,0,1,1])
+    plt.savefig("Color_Pair_reference_manual.pdf",bbox_inches='tight')
+    plt.show()
